54a937619c241f21f348463a0f520aa8961babc3
chase carney <ccarney36@yahoo.com>
2023-05-14 20:21:30 -0400

    UPDATED PARTIALS

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,6 @@
 const travel = (req, res) => {
-    pageTitle = process.env.npm_package_description + ' - Travel';
-    res.render('travel', {title: pageTitle});
+    //pageTitle = process.env.npm_package_description + ' - Travel';
+    res.render('travel', {title: 'Travlr Getaways'});
 };
 
 module.exports = {
