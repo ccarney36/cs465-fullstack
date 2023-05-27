c47dec36f20ff24d818858960814ecdc9c2f8087
chase carney <ccarney36@yahoo.com>
2023-05-26 21:17:33 -0400

    Module 4 MongoDB data created

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,7 +1,10 @@
+var fs = require('fs');
+var mealsData = JSON.parse(fs.readFileSync('./data/meals.json', 'utf8'));
+
 /* GET meals page */
 const meals = (req, res) => {
     pageTitle = 'Travlr Getaways - Meals';
-    res.render('meals', {title: pageTitle});
+    res.render('meals', {title: pageTitle, mealsData});
 };
 module.exports = {
     meals
--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,7 +1,10 @@
+var fs = require('fs');
+var roomsData = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf8'));
+
 /* GET rooms page */
 const rooms = (req, res) => {
     pageTitle = 'Travlr Getaways - Rooms';
-    res.render('rooms', {title: pageTitle});
+    res.render('rooms', {title: pageTitle, roomsData});
 };
 module.exports = {
     rooms
